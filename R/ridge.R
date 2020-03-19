49859fa27b37f2e91d880eb12bde55572dbc4a38
Dan Frankowski <dfrankow@gmail.com>
2020-03-18 22:00:55 -0500

    Finish NAMESPACE generation with package-level docs

---
+++ R/ridge.R
@@ -0,0 +1,13 @@
+## Package-level documentation
+## See https://r-pkgs.org/man.html#man-packages
+
+#' ridge: Linear and logistic ridge regression functions.
+#' 
+#' Additionally includes special functions for 
+#' genome-wide single-nucleotide polymorphism (SNP) data.
+#' 
+#' @docType package
+#' @name ridge
+#' @useDynLib ridge, .registration=TRUE
+NULL
+#> NULL
