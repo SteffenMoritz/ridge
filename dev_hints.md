8857099ef362885961b43113de525488d27c9f75
SteffenMoritz <steffen.moritz10@gmail.com>
2021-01-28 03:55:26 +0100

    configure.ac update

---
+++ dev_hints.md
@@ -0,0 +1,3 @@
+When preparing an update and changing things in configure.ac
+
+Run path/ridge/autoconf in console to bring changes to configure file
--- src/config.h
+++ src/config.h
@@ -1,4 +1,4 @@
 /* src/config.h.  Generated from config.h.in by configure.  */
 /* Define as 1 if GSL is found */
 
-#define HAVE_GSL_HEADER 1
+#define HAVE_GSL_HEADER /**/
