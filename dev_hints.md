c3872f93fc6191d485705c3f74d07fd034d1e0e9
SteffenMoritz <steffen.moritz10@gmail.com>
2021-10-10 03:37:03 +0200

    Updated autoconf to 2.71

--- dev_hints.md
+++ dev_hints.md
@@ -1,3 +1,8 @@
+Install latest autoconf version before next steps (also run autoupdate)
+
 When preparing an update and changing things in configure.ac
 
 Run path/ridge/autoconf in console to bring changes to configure file
+Do changes to configure.ac - running path/ridge/autoconf then creates the configure file
+
+configure file is not updated automatically with build()
--- src/config.h
+++ src/config.h
@@ -1,4 +1,56 @@
 /* src/config.h.  Generated from config.h.in by configure.  */
-/* Define as 1 if GSL is found */
+/* src/config.h.in.  Generated from configure.ac by autoheader.  */
 
+/* Define to 1 if you have the <gsl/gsl_version.h> header file. */
+#define HAVE_GSL_GSL_VERSION_H 1
+
+/* Description */
 #define HAVE_GSL_HEADER /**/
+
+/* Define to 1 if you have the <inttypes.h> header file. */
+#define HAVE_INTTYPES_H 1
+
+/* Define to 1 if you have the <memory.h> header file. */
+#define HAVE_MEMORY_H 1
+
+/* Define to 1 if you have the <stdint.h> header file. */
+#define HAVE_STDINT_H 1
+
+/* Define to 1 if you have the <stdlib.h> header file. */
+#define HAVE_STDLIB_H 1
+
+/* Define to 1 if you have the <strings.h> header file. */
+#define HAVE_STRINGS_H 1
+
+/* Define to 1 if you have the <string.h> header file. */
+#define HAVE_STRING_H 1
+
+/* Define to 1 if you have the <sys/stat.h> header file. */
+#define HAVE_SYS_STAT_H 1
+
+/* Define to 1 if you have the <sys/types.h> header file. */
+#define HAVE_SYS_TYPES_H 1
+
+/* Define to 1 if you have the <unistd.h> header file. */
+#define HAVE_UNISTD_H 1
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "steffen.moritz10@gmail.com"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "ridge"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "ridge version-3.0"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "ridge_3.0.tar.gz"
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL "http://cran.r-project.org/web/packages/ridge"
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "version-3.0"
+
+/* Define to 1 if you have the ANSI C header files. */
+#define STDC_HEADERS 1
