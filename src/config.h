9f31b5d8b7ecd0b18a9f536a39d55e18b8d1ac23
Steffen Moritz <steffen.moritz10@gmail.com>
2022-02-07 20:59:13 +0100

    3.1 update final steps

--- src/config.h
+++ src/config.h
@@ -10,11 +10,11 @@
 /* Define to 1 if you have the <inttypes.h> header file. */
 #define HAVE_INTTYPES_H 1
 
-/* Define to 1 if you have the <memory.h> header file. */
-#define HAVE_MEMORY_H 1
-
 /* Define to 1 if you have the <stdint.h> header file. */
 #define HAVE_STDINT_H 1
+
+/* Define to 1 if you have the <stdio.h> header file. */
+#define HAVE_STDIO_H 1
 
 /* Define to 1 if you have the <stdlib.h> header file. */
 #define HAVE_STDLIB_H 1
@@ -41,16 +41,18 @@
 #define PACKAGE_NAME "ridge"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "ridge version-3.0"
+#define PACKAGE_STRING "ridge version-3.1"
 
 /* Define to the one symbol short name of this package. */
-#define PACKAGE_TARNAME "ridge_3.0.tar.gz"
+#define PACKAGE_TARNAME "ridge_3.1.tar.gz"
 
 /* Define to the home page for this package. */
 #define PACKAGE_URL "http://cran.r-project.org/web/packages/ridge"
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "version-3.0"
+#define PACKAGE_VERSION "version-3.1"
 
-/* Define to 1 if you have the ANSI C header files. */
+/* Define to 1 if all of the C90 standard headers exist (not just the ones
+   required in a freestanding environment). This macro is provided for
+   backward compatibility; new code need not use it. */
 #define STDC_HEADERS 1
